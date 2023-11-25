52d5dc37d2766b75e28550410b79c7baee37971a
Kevram73 <eyramativon1@gmail.com>
2023-11-25 21:07:44 +0100

    First commit

--- service/__init__.py
+++ service/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['STATIC_FOLDER'] = "./static/"
 app.config['SECRET_KEY'] = 'secret_key'
 
 db = SQLAlchemy(app)
